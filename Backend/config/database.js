117beb24aa9cbee659c3e2830a7f2e85b90acb4c
Rimjhim Jain <rimjhimjain2973@gmail.com>
2024-07-03 00:59:01 +0530

    chnges

---
+++ Backend/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = ()=>{
+    mongoose.connect(process.env.DB_URI).then(
+        (data)=>{
+        console.log(`Mongodb connected to server ${data.connection.host}`);
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+module.exports = connectDB
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-
+const connectDB = require("./config/database");
 dotenv.config({path:"Backend/config/config.env"});
 
+// connecting to DB
+connectDB();
 app.listen(process.env.PORT,() => {
     console.log(`Server is working on https://localhost:${process.env.PORT}`)
 })
