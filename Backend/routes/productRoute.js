b2fd041aaab1f353d9c82b71d0cd1fdb2b2d710f
Rimjhim Jain <rimjhimjain2973@gmail.com>
2024-07-03 19:48:30 +0530

    done

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { getAllProducts } = require("../controlers/productController");
+const { getAllProducts , createProduct, updateProduct, deleteProduct} = require("../controlers/productController");
 
 
 const router = express.Router();
 router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
+router.route("/product/:id").delete(deleteProduct);
 module.exports = router
--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,11 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDB = require("./config/database");
 
+
 dotenv.config({path:"Backend/config/config.env"});
-PORT = 6969;
+
 // connecting to DB
 connectDB();
 app.listen(process.env.PORT,() => {
     console.log(`Server is working on https://localhost:${process.env.PORT}`)
 })
-// runnnnn ??????? bas ?? agyi error 
