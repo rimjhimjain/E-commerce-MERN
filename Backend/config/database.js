199e2e3455d825459624a2d83cb1932868017ac8
Rimjhim Jain <rimjhimjain2973@gmail.com>
2024-07-03 13:34:39 +0530

    productmodel done

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,12 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectDB = ()=>{
-    mongoose.connect(process.env.DB_URI).then(
-        (data)=>{
-        console.log(`Mongodb connected to server ${data.connection.host}`);
-    }).catch((err)=>{
-        console.log(err)
-    })
-}
+MONGO_URI = "mongodb+srv://admin:2134@cluster0.peaqjxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+const connectDB = async () => {
+    try {
+        const coon = await mongoose.connect(MONGO_URI);
+        console.log("Mongodb is connected");
+
+    } catch (error) {
+        console.error(`Error ${error.message}`);
+        process.exit(1);
+    }
+
+};
 module.exports = connectDB
