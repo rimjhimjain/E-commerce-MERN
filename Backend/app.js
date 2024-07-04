4f728c3ae358389abd2dd03a2f59892814e0220f
Rimjhim Jain <rimjhimjain2973@gmail.com>
2024-07-04 12:31:13 +0530

    done

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
 const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+// const fileUpload = require("express-fileupload");
+const path = require("path");
+
+
 app.use(express.json());
-//Route import
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(fileUpload());
+
+// Route Imports
 const product = require("./routes/productRoute");
-app.use("/api/v1",product);
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 //midleware for err
 app.use(errorMiddleware);
